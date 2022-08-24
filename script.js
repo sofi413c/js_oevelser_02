0096e82de1ff7120045339456a41be80c81f81df
Sofie Hemmingsen <sofi413c@stud.kea.dk>
2022-08-24 11:56:18 +0200

    oevelse 2B done

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const overskrift = document.querySelector("h1");
-console.log(overskrift);
+
+//ØVELSE 2B//
+overskrift.textContent;
+console.log(overskrift.textContent);
+overskrift.textContent = "Billeder"; //Gør at du kan redigerer i tekstcontent gennem js
+
 const underoverskrift = document.querySelector("h2");
 console.log(underoverskrift);
 const lilleo = document.querySelector("h3");
 console.log(lilleo);
-const tekst = document.querySelector("p");
-console.log(tekst);
+
+const article1 = document.querySelector("article:nth-child(1) p ");
+console.log(article1);
+article1.innerHTML = "Her er et billede af en mand der spiller <b>Golf<b>";
+
+const article2 = document.querySelector("article: nth-child(2) p");
+console.log(article2);
+article2.textContent = "Her er et billede af en telefon";
+
 const footer = document.querySelector("footer");
 console.log(footer);
-
-
