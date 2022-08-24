e839addb62b655ee007083eb14dfe76b70af10db
Sofie Hemmingsen <sofi413c@stud.kea.dk>
2022-08-24 10:44:03 +0200

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+const overskrift = document.querySelector("h1");
+console.log(overskrift);
+const underoverskrift = document.querySelector("h2");
+console.log(underoverskrift);
+const lilleo = document.querySelector("h3");
+console.log(lilleo);
+const tekst = document.querySelector("p");
+console.log(tekst);
+const footer = document.querySelector("footer");
+console.log(footer);
